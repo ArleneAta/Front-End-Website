55bbcbe4830a5acbc13e70c7d8e7d895b1dfa19a
Jan Lingat <ajlingat@gmail.com>
2018-04-04 15:05:27 -0700

    added program page

--- dev/program.php
+++ dev/program.php
@@ -1,12 +1,54 @@
 <?php
     include("header.php");
 ?> 
-<main class="program">
+<main class="program-main">
 
-    <h1>Program</h1>
+    <section class="background program-img" id="program">
+    <div class="container">
+        <h1>Program</h1>
+    </div>
+    </section>
+    <section class="program-overview">
+        <p>The Software Systems Developer (SSD) program (Web Programmer option) is an eight month, full-time business application development program offered by BCIT's School of Computing and Academic Studies, within the HTP department at the downtown Vancouver campus. 
+        It prepares individuals for high-demand careers in the technical areas of web application development, programming, database and systems analysis. Students learn industry-ready skills through extensive hands-on practice in current scripting and coding techniques, object-oriented architecture patterns, application security and scalability, developer tools, and platforms for building complete web-based business applications from scratch. 
+        Graduates of this certificate program may apply for acceptance into the BCIT Bachelor of Technology in Computer Systems (additional prerequisites are required). 
+        This program is delivered through classroom instruction, hands-on lab work and individual and team projects. While applicants are not required to have formal computer programming experience, they need to have a solid understanding of object oriented programming as well as possess sound logical and reasoning skills. Applicants with no knowledge are advised to take a programming language course, such as introduction to object-oriented programming, C#, or Java.</p>
+        <hr>
+    </section>
+    <section class="program-application">
 
+    
+    <article class="program-entry">
+        <h3>Program Entry</h3>
+        <h4>Entrance requirements</h4>
+        <p>This program has a two-step admission process. Applicants must meet all entrance requirements and will be accepted on a first qualified basis as long as space remains.</p>
+        <span>
+        <ol>
+            <li>Complete the pre-entry assessment form</li>
+            <ul>
+                <li><a href="https://www.bcit.ca/files/cas/htp/pdf/htp-7_a_ssd_pre-entry.pdf">Pre-Entry Assessment PDF</a></li>
+            </ul>
+            <li>Meet the following entrance requirements</li>
+            <ul>
+                <li>English 12 (50)</li>
+                <li>Programming</li>
+                <ul>
+                    <li>COMP 1409</li> or
+                    <li>3.0 credits of post-secondary object-oriented programming from a recognized institution (70%)</li>
+                </ul>
+            </ul>
+        </ol>
+        </span>
+    </article>
 
+    <article class="program-intake">
+        <h3>Program Intake</h3>
+        <p>September of each year.</p>
+        <p>Please see the <a href="https://www.bcit.ca/study/programs/699ccertt">program overview</a> for precise dates.</p>
 
+        <a href="https://secure.bcit.ca/sis/apply/" class="btn btn-default" style="text-decoration:none; margin-left:35%; margin-top: 10%;">APPLY NOW</a>
+    </article>
+    </section>
 
 </main>
 <?php
