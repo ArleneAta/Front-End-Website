86413de28682333f10ff392127ad389b09d7ade5
Arlene Ata <arlene.ata@gmail.com>
2018-04-04 11:17:59 -0700

    data & student page layout
    add data files and student page layout styling still needed

--- dev/students.php
+++ dev/students.php
@@ -3,8 +3,24 @@
 ?> 
 <main class="students">
 
-    <h1>Students</h1>
+    <h1>Current Students</h1>
 
+    <div class="students-container">
+        
+        <!-- read students csv file and place into grid items -->
+        <?php
+            $csvFile = file('./data/ssd-student-list-2017-2018.csv');
+            $students = [];
+            foreach ($csvFile as $one) {
+                //echo "<p>$one</p>"; - prints out csv as shown
+                $students[] =str_getcsv($one);               
+            }
+
+            foreach ($students as $stud){
+                echo '<div class="grid-item"> <p>'. $stud[1] . ' ' . $stud[0] . '</p><p>'. $stud[2].'</p></div>';
+            }
+        ?>   
+    </div>
 
 
 
