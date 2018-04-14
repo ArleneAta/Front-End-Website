f77d02c0f051f5b94136b1873a88b35de2a14300
Jan Lingat <ajlingat@gmail.com>
2018-04-13 17:13:06 -0700

    changes in nav
    - edited nav bar and nav bar styles

--- dev/header.php
+++ dev/header.php
@@ -5,8 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>SSD Website</title>
-<<<<<<< HEAD
-<<<<<<< HEAD
     <link rel="stylesheet" href="./styles/styles.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script>
@@ -19,26 +17,10 @@
 			}
 		}
 	</script>
-	
-=======
-=======
->>>>>>> 830088ab2ae5ce6f184b55e106c281b37abb9445
-	<link rel="stylesheet" href="./styles/styles.css">
-	<link rel="stylesheet" href="./css/calendar.css">
-	<link rel="stylesheet" href="./css/custom_1.css">
-	<link rel="stylesheet" href="./css/demo.css">
-	<script src="./js/show-nav.js"></script>   
-	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<<<<<<< HEAD
->>>>>>> 830088ab2ae5ce6f184b55e106c281b37abb9445
-=======
->>>>>>> 830088ab2ae5ce6f184b55e106c281b37abb9445
 </head>
 <body>
 <div class="wrapper">
     <header>
-<<<<<<< HEAD
-<<<<<<< HEAD
 		<div class="topnav" id="myTopnav">
 			<a href="./index.php" class="logo">
 					<img src="images/bcit-logo.png" alt="BCIT Logo">
@@ -55,36 +37,3 @@
 		</div>
 	</div>
 	</header>
-	
-=======
-=======
->>>>>>> 830088ab2ae5ce6f184b55e106c281b37abb9445
-	<button class="hamburger">
-			<div class="hamburger-content">
-				<span class="hamburger-icon" tabindex="-1">
-						&#9776;
-				</span>
-				<span class="hamburger-text">
-					Menu
-				</span>
-			</div>
-		</button>
-		<nav id="menu" class="main-nav nav">
-			<div class="container">
-			<div class="branding"> 
-				<a href="./index.php" class="logo">
-				<img src="images/bcit-logo.png" alt="BCIT Logo">
-				</a>
-			</div><!-- branding div -->
-			<ul>
-				<li><a href="./program.php">Program</a></li>
-				<li><a href="./schedule.php">Schedule</a></li>
-				<li><a href="./students.php">Students</a></li>
-				<li><a href="./staff.php">Staff</a></li>
-				<li><a href="./eresources.php">E-Resources</a></li>
-				<li><a href="./contact.php">Contact</a></li>
-			</ul>
-			</div>	<!-- container div -->
-		</nav>
-	</header>
->>>>>>> 830088ab2ae5ce6f184b55e106c281b37abb9445
